7fa077d6f56c1df32f19ebe4dd23f22ce2eb6944
Danil Lebets <stebel99@gmail.com>
2020-04-22 15:34:07 +0300

    add flower controller with views using EF

--- Practice5.WebUI/Controllers/HomeController.cs
+++ Practice5.WebUI/Controllers/HomeController.cs
@@ -12,19 +12,5 @@ namespace Practice5.WebUI.Controllers
         {
             return View();
         }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
